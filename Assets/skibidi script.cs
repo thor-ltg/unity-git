340dc0001fc2ada07f4cd5deceb97d14bd8477b7
Thor Grimes <thools0731@staden.gotheborg.net>
2025-01-09 13:27:59 +0100

    la till pink alien och en platform

--- Assets/skibidi script.cs
+++ Assets/skibidi script.cs
@@ -3,14 +3,21 @@ using UnityEngine;
 public class skibidiscript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Rigidbody2D rb;
+    public float walkspeed = 6;
+    public float jumpspeed = 11;
     void Start()
     {
-        
+        rb=GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.linearVelocityX = Input.GetAxisRaw("Horizontal") * walkspeed;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.linearVelocityY = jumpspeed;
+        }
     }
 }
