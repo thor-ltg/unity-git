8e06469ff2e63fdbe4c594a384f8b2262e4684b8
Thor Grimes <thools0731@staden.gotheborg.net>
2025-01-30 11:50:51 +0100

    gjorde så snailtrigger funkar på vanliga snails

--- Assets/Scripts/SnailScript.cs
+++ Assets/Scripts/SnailScript.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class SnailScript : MonoBehaviour
 {
+    public  float movespeed = -0.005f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,13 +12,19 @@ public class SnailScript : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position += new Vector3(-0.005f, 0);
+        transform.position += new Vector3(movespeed, 0);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Fireball")
         {
             Destroy(gameObject);
+        }
+        if (collision.tag == "SnailTrigger")
+        {
+            movespeed *= -1;
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.flipX = !spriteRenderer.flipX;
         }
     }
 }
